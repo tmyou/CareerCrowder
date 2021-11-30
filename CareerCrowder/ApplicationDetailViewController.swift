a48631cb5c5be3eed331d1ba7ce02d7dcb571548
Alan Frank <alanfrank1998@gmail.com>
2021-11-30 13:44:46 -0600

    Updated drop down buttons for both application pages

--- CareerCrowder/ApplicationDetailViewController.swift
+++ CareerCrowder/ApplicationDetailViewController.swift
@@ -7,12 +7,35 @@
 
 import UIKit
 import CoreData
+//import DropDown
+
+// ------------------------------------------------------------------------
+
+class CellClass: UITableViewCell {
+    
+}
+
+// ------------------------------------------------------------------------
 
 protocol CreateApplication {
     func addApp(name: String, jobTitle: String, status: String, locationAddress: String, link: String, dateApp: Date, salary: String, Desc: String)
     func editApp(name: String, jobTitle: String, status: String, locationAddress: String, link: String, dateApp: Date, salary: String, Desc: String, index: Int, selectedAppToEdit: Applications)
 }
 class ApplicationDetailViewController: UIViewController {
+    
+    // ------------------------------------------------------------------------
+    
+    @IBOutlet var btnSelectStatus: UIButton!
+    
+    
+    let transparentView = UIView()
+    let ststableView = UITableView()
+    
+    var selectedButton = UIButton()
+    
+    var dataSource = [String]()
+    
+    // ------------------------------------------------------------------------
     
     @IBOutlet weak var companyName: UITextField!
     
@@ -58,6 +81,15 @@ class ApplicationDetailViewController: UIViewController {
             backBarBtnItem.title = "something different"
             navigationItem.backBarButtonItem = backBarBtnItem
         // Do any additional setup after loading the view.
+        
+        // ------------------------------------------------------------------------
+        
+        ststableView.delegate = self
+        ststableView.dataSource = self
+        ststableView.register(CellClass.self, forCellReuseIdentifier: "Cell")
+        btnSelectStatus.titleLabel?.font = UIFont(name: "Lato-Regular", size: 25)
+        
+        // ------------------------------------------------------------------------
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -67,21 +99,30 @@ class ApplicationDetailViewController: UIViewController {
     }
     
     @IBAction func completeApplication(_ sender: Any) {
-                if companyName.text?.count == 0 || JobTitle.text?.count == 0 || Location.text?.count == 0 || Status.text?.count == 0
-                {
-                    let alert = UIAlertController(title: "Empty Text", message: "Please fill out all text fields", preferredStyle: .alert)
+        if companyName.text?.count == 0 || JobTitle.text?.count == 0 || Location.text?.count == 0                 {
+                    let alert = UIAlertController(title: "Empty Text Field", message: "Please fill out all text fields", preferredStyle: .alert)
                     alert.addAction(UIAlertAction(title: "Okay", style: .cancel, handler: { _ in self.dismiss(animated: true, completion: nil)}))
                     self.present(alert, animated: true, completion: nil)
                 }
-                else if !dayArr.contains(Status.text!) {
-                    let alert = UIAlertController(title: "Invalid Status", message: "Please select a valid status from the button! e.g. Applied", preferredStyle: .alert)
-                    alert.addAction(UIAlertAction(title: "Okay", style: .cancel, handler: { _ in self.dismiss(animated: true, completion: nil)}))
-                    self.present(alert, animated: true, completion: nil)
-                }
+//        else if !dayArr.contains((selectedButton.titleLabel?.text)!) {
+//                    let alert = UIAlertController(title: "Invalid Status", message: "Please select a valid status from the button! e.g. Applied", preferredStyle: .alert)
+//                    alert.addAction(UIAlertAction(title: "Okay", style: .cancel, handler: { _ in self.dismiss(animated: true, completion: nil)}))
+//                    self.present(alert, animated: true, completion: nil)
+//                }
+        else if btnSelectStatus.titleLabel?.text == "Select Status..." {
+            let alert = UIAlertController(title: "Select Status", message: "Please select an application status", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Okay", style: .cancel, handler: { _ in self.dismiss(animated: true, completion: nil)}))
+            self.present(alert, animated: true, completion: nil)
+        }
                else{
                     name = companyName.text!
                     jobTitle = JobTitle.text!
-                    statusTitle = Status.text!
+//                    statusTitle = Status.text!
+                   // ------------------------------------------------------------------------
+                    //statusTitle = (selectedButton.titleLabel?.text)!
+                   statusTitle = (selectedButton.titleLabel?.text)!
+                   //print(statusTitle)
+                   // ------------------------------------------------------------------------
                     locationTitle = Location.text!
                     link = jobLink.text!
                     sal = salary.text!
@@ -104,7 +145,42 @@ class ApplicationDetailViewController: UIViewController {
         self.present(actionSheetAlert, animated: true, completion: nil)
     }
     
+    // ------------------------------------------------------------------------
     
+    func addTransparentView(frames: CGRect) {
+        let window = UIApplication.shared.keyWindow
+        transparentView.frame = window?.frame ?? self.view.frame
+        self.view.addSubview(transparentView)
+        
+        ststableView.frame = CGRect(x: frames.origin.x, y: frames.origin.y + frames.height, width: frames.width, height: 0)
+        self.view.addSubview(ststableView)
+        ststableView.layer.cornerRadius = 5
+        
+        transparentView.backgroundColor = UIColor.black.withAlphaComponent(0.9)
+        ststableView.reloadData()
+        let tapgesture = UITapGestureRecognizer(target: self, action: #selector(removeTransparentView))
+        transparentView.addGestureRecognizer(tapgesture)
+        transparentView.alpha = 0
+        UIView.animate(withDuration: 0.4, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveEaseInOut, animations: {self.transparentView.alpha = 0.5
+            self.ststableView.frame = CGRect(x: frames.origin.x, y: frames.origin.y + frames.height + 5, width: frames.width, height: 150)
+        }, completion: nil)
+    }
+    
+    @objc func removeTransparentView() {
+        let frames = selectedButton.frame
+        UIView.animate(withDuration: 0.4, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveEaseInOut, animations: {self.transparentView.alpha = 0.0
+            self.ststableView.frame = CGRect(x: frames.origin.x, y: frames.origin.y + frames.height, width: frames.width, height: 0)
+        }, completion: nil)
+    }
+    
+    @IBAction func onClickSelectStatus(_ sender: Any) {
+        dataSource = ["Need to Apply", "Applied", "Interviewing", "Offered", "Rejected"]
+        selectedButton = btnSelectStatus
+        addTransparentView(frames: btnSelectStatus.frame)
+        //selectedButton.titleLabel?.font = UIFont(name: "Lato-Regular", size: 25)
+    }
+    
+    // ------------------------------------------------------------------------
     
 
     /*
@@ -118,3 +194,33 @@ class ApplicationDetailViewController: UIViewController {
     */
 
 }
+
+// ------------------------------------------------------------------------
+
+extension ApplicationDetailViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = ststableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = dataSource[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let attributedTitle = btnSelectStatus.attributedTitle(for: .normal) {
+            let mutableAttributedTitle = NSMutableAttributedString(attributedString: attributedTitle)
+            mutableAttributedTitle.replaceCharacters(in: NSMakeRange(0, mutableAttributedTitle.length), with: dataSource[indexPath.row])
+            btnSelectStatus.setAttributedTitle(mutableAttributedTitle, for: .normal)
+            //btnSelectStatus.setTitleColor(UIColor.red, for: .normal)
+            //btnSelectStatus.titleLabel?.font = UIFont(name: "Lato-Regular", size: 25)
+        }
+        //selectedButton.titleLabel?.font = UIFont(name: "Lato-Regular", size: 25)
+        selectedButton.setTitle(dataSource[indexPath.row], for: .normal)
+        print(dataSource[indexPath.row])
+        removeTransparentView()
+    }
+}
+
+// ------------------------------------------------------------------------
